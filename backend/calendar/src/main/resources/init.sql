a8380ab48de86b631803d53ba9ecb19b967ad3d6
mako-makok <lmk19960420@gmail.com>
2021-05-22 12:18:36 +0900

    初期化sqlを追加

--- backend/calendar/src/main/resources/init.sql
+++ backend/calendar/src/main/resources/init.sql
@@ -24,3 +24,28 @@ create table plan (
 );
 create sequence plan_id_seq;
 
+-- test data
+insert into account(id, first_name, last_name)
+values
+    (nextval('account_id_seq'), 'test', '1'),
+    (nextval('account_id_seq'), 'test', '2'),
+    (nextval('account_id_seq'), 'test', '3'),
+    (nextval('account_id_seq'), 'test', '4'),
+    (nextval('account_id_seq'), 'test', '5')
+;
+
+insert into calendar(id, name, account_id)
+values
+    (nextval('calendar_id_seq'), 'テスト', 1),
+    (nextval('calendar_id_seq'), '自分の', 1),
+    (nextval('calendar_id_seq'), 'トモダチ1', 3),
+    (nextval('calendar_id_seq'), 'トモダチ2', 1),
+    (nextval('calendar_id_seq'), 'トモダチ3', 2)
+;
+
+insert into plan(id, calendar_id, title, description, place, start_time, end_time)
+select
+       nextval('plan_id_seq'), 1, to_char(cur, 'YYYY/MM/DD'), '', '', cur, cur + '3 hours'
+from
+     generate_series(now(), now() + '1 months', '1 days') as cur
+;
